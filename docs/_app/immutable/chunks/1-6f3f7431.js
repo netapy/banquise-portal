3ef3e773e119d53202e43df9ec7c7d4c7a7b1800
netapy <39877685+netapy@users.noreply.github.com>
2022-11-14 12:47:03 +0100

    update

---
+++ docs/_app/immutable/chunks/1-6f3f7431.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-a4841987.js";export{t as component};
--- docs/_app/immutable/chunks/2-82bc4ab8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-7b036b5b.js";export{t as component};
