81307f6a7bc8b6952513cca44261831243c1cc1a
netapy <39877685+netapy@users.noreply.github.com>
2022-11-14 12:39:30 +0100

    Revert "update"
    This reverts commit ac5ecc58afce0aaa23624f0a62c8030f6d8d8865.

---
+++ docs/_app/immutable/chunks/1-06123e60.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ff7f90cc.js";export{t as component};
--- docs/_app/immutable/chunks/2-4f391f73.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-469afdc3.js";export{t as component};
